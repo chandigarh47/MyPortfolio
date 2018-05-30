ed6297cb6da32e1b72338341ca2bcfbc506d5824
gurdeep kaur <gurdeepkaur@192-168-1-197.tpgi.com.au>
2018-05-30 21:19:07 +1000

    paragarph about me

--- js/main.js
+++ js/main.js
@@ -56,9 +56,63 @@ $(document).on('ready', function () {
       $('.menuContent').toggleClass( "show" );
     })
 
-    
-
-
-
-
   });
+
+  var letterDelay=25; // Delay between individual letter fade-ins.
+var letterSpeed=250; // How quickly does each letter fade in?
+
+
+$(document).ready(function() {
+
+  // Store the html as a string.
+  var text=$('.paragraph').html();
+
+  // Break the characters up into an array.
+  var letters=[];
+  for (var i=0;i<text.length;i++) {
+    letters.push(text[i]);
+  }
+
+  // Clear the current html
+  $('.paragraph').html('');
+
+  // Make sure the paragraph element is visible
+  $('.paragraph').css({display:'block'})
+
+  // Iterate through the just-created letters array
+  for (var i=0;i<letters.length;i++) {
+
+    // Create the element
+    var el=$("<div class='letter'>"+letters[i]+"</div>");
+    el.css({
+      display:'inline',
+      float:'left',
+      position:'relative',
+      opacity:0,
+      top:'-5px'
+    })
+
+    // Trigger the fade-in animation
+    enterLetter(el,i*letterDelay);
+
+    // Place it in the paragraph element
+    $('.paragraph').append(el);
+
+    // Force spaces
+    if (letters[i]===' ' && i!==0) {
+      el.append("&nbsp;");
+    }
+  }
+})
+
+// Right now the animation just pulls the letter down and
+// makes it fully visible. But this could be any animation
+// you please.
+function enterLetter(letter,delay) {
+  setTimeout(function() {
+    letter.animate({
+      opacity:1,
+      top:0
+    }, {duration:letterSpeed, queue:false})
+  },delay)
+}
